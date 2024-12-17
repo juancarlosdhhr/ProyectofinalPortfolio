79de9ffe9e437e744a83862c3f02b0d8e6f4a484
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-12-17 03:11:50 +0000

    update

--- main.js
+++ main.js
@@ -16,16 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-/* import { changeTheme } from "./components/Navbar/Navbar";
-import { linkPage } from "./utils/linkPage";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Footer } from "./components/Footer/Footer";
-import { Home } from "./pages/Home/Home";
-import { Projects } from "./pages/Projects/Projects";
-import { Divider } from "./components/Divider/Divider";
-
- */
-
 //Y ahora podemos hacer una función adicional (opcional):
 
 const create = () => {
